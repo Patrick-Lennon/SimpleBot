48ec5402c853cdd99ed9256182b8cdc8968e5d44
Patrick Lennon <patlennonri@gmail.com>
2018-06-20 21:40:04 -0400

    Added comments, and added retweet feature

--- Codsworth.py
+++ Codsworth.py
@@ -1,13 +1,18 @@
+"""
+Program to like and/or retweet all the tweets within a certain amount that contain key words
+So that I'll never miss an important update or anything else on current events, favorite teams, upcoming games, etc.
+
+# author: Patrick Lennon
+"""
+
 from _tkinter import *
-import pip
-package_name='tweepy'
-pip.main(['install', package_name])
+import tweepy
 
 
-consumer_key = 'your consumer key'
-consumer_secret = 'your consumer secret key'
-access_token = 'your access token'
-access_token_secret = 'your access secret token'
+consumer_key = 'your info'
+consumer_secret = 'your info'
+access_token =  'your info'
+access_token_secret =  'your info'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -15,6 +20,7 @@ api = tweepy.API(auth)
 
 root = Tk()
 
+#making labels for the GUI
 label1 = Label(root, text="Search")
 E1 = Entry(root, bd=5)
 
@@ -24,14 +30,37 @@ E2 = Entry(root, bd=5)
 label3 = Label(root, text="Favorite")
 E3 = Entry(root, bd=5)
 
+label4 = Label(root, text="Retweeet")
+E4 = Entry(root, bd=5)
 
+#Functions returning the text for the boxes in the gui
+def getE1():
+    return E1.get()
+
+def getE2():
+    return E2.get()
+
+def getE3():
+    return E3.get()
+
+def getE4():
+    return E4.get()
+
+#main function
 def doWork():
+    getE1()
     search = getE1()
 
+    getE2()
     numberOfTweets = getE2()
+    #have to convert the number into an int
     numberOfTweets = int(numberOfTweets)
 
+    getE3()
     fav = getE3()
+
+    getE4()
+    re = getE4()
 
 
     if fav == "yes" or fav == "Yes":
@@ -43,7 +72,20 @@ def doWork():
             except StopIteration:
                 break
 
+    if re == "yes" or re == "Yes":
+        for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+            try:
+                # Retweet
+                tweet.retweet()
+                print('Retweeted the tweet')
 
+            except tweepy.TweepError as e:
+                print(e.reason)
+
+            except StopIteration:
+                break
+
+#packing each label so it doesn't disappear
 label1.pack()
 E1.pack()
 
@@ -53,14 +95,8 @@ E2.pack()
 label3.pack()
 E3.pack()
 
-def getE1():
-    return E1.get()
-
-def getE2():
-    return E2.get()
-
-def getE3():
-    return E3.get()
+label4.pack()
+E4.pack()
 
 submit = Button(root, text="Submit", command = doWork)
 
